8ee76e57137c14e9dff8e8b0f26040abcad760b1
Luke Hayes <lukehayesa@gmail.com>
2022-04-28 01:05:51 -0400

    first batch

---
+++ my-app/src/App.jsx
@@ -0,0 +1,78 @@
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+import "./App.css"
+import { useEffect } from "react";
+import NewPost from "./components/NewPost";
+
+const App = () => {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image()
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+      setImage ({
+        url: img.src,
+        width: img.width,
+        height: img.height,
+      });  
+      }
+    };
+
+    file && getImage();
+  }, [file]);
+
+  return (
+    <div>
+      <Navbar />
+      {image ? (<NewPost image={image}/>) : (
+
+      
+      <div className="newPostCard">
+        <div className="addPost">
+          <img
+          src="https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?cs=srgb&dl=pexels-mentatdgt-937481.jpg&fm=jpg"
+          alt=""
+          className="avatar"
+          />
+          <div className="postForm">
+            <input
+            type="text"
+            placeholder="Whatcha Thinking?"
+            className="postInput"
+            />
+
+            <label htmlFor="file">
+
+                <img
+                className="addImg"
+                src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                alt=""
+                />
+                <img
+                className="addImg"
+                src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                alt=""
+                />
+                <img
+                className="addImg"
+                src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                alt=""
+                />
+                <button>Post</button>
+            </label>
+            <input onChange={(e) => setFile(e.target.files[0])}
+              id="file" 
+              style={{display:"none"}} 
+              type="file"
+              />
+          </div>
+        </div>
+      </div>
+      )}
+    </div>
+  );
+}
+export default App;
